dd651a00cd7fa5e85589b14c819af78497bae77a
syab-syab <syabsyabsyabsyab@gmail.com>
2023-10-25 22:45:16 +0900

    ロジックその他は明日

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1 className="title">効果音ボタン</h1>
       </header>
+
+      <main className="container">
+        <select name="" id="sound-select" className="sound-select">
+          <option value="" selected>選択してください。</option>
+          <option value="0">声系1</option>
+          <option value="1">声系2</option>
+          <option value="2">SE1</option>
+          <option value="3">SE2</option>
+          <option value="4">SE3</option>
+          <option value="5">悪用禁止!!</option>
+        </select>
+        <div className="sound-effect-button-space" id="sound-effect-button-space">
+          ボタン表示スペース
+        </div>
+      </main>
+
+      <footer className="footer">
+        {/* <div className="twitter-share-btn">
+          <a href="http://twitter.com/share?url=https://syab-syab.github.io/soundEffectBtn/&text=効果音ボタン" target="_blank"><i className="fa-brands fa-twitter"></i><span>Twitter</span></a>
+        </div> */}
+        <span className="copy">
+          © 効果音ボタン
+        </span>
+      </footer>
     </div>
   );
 }
